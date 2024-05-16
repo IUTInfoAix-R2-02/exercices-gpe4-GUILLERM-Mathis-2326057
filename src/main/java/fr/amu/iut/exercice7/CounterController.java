e8828701103ca8bde3628078000bce32462e90ef
GUILLERM Mathis <GUILLERM@gmail.com>
2024-05-16 12:00:27 +0200

    exo 7

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,18 +1,39 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
 
-    int counter = 0;
+    @FXML
+    private Label counterLabel;
 
+    @FXML
+    private Button incrementButton;
+
+    @FXML
+    private Button decrementButton;
+
+    private int counter = 0;
+
+    
+
+    @FXML
     public void increment() {
+        counter++;
+        counterLabel.setText(Integer.toString(counter));
     }
 
+    @FXML
     public void decrement() {
+        counter--;
+        counterLabel.setText(Integer.toString(counter));
     }
 
     @Override
