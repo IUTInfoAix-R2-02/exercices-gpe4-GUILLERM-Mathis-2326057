aa2df5d75b715f943caaa3b1340151c1cb3a3d1a
GUILLERM Mathis <GUILLERM@gmail.com>
2024-05-29 10:44:16 +0200

    finitions

--- src/main/java/fr/amu/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/amu/iut/exercice6/IHMPendu.java
@@ -1,162 +1,180 @@
 package fr.amu.iut.exercice6;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.HashSet;
+import java.util.Set;
 
 public class IHMPendu extends Application {
 
+    private Label livesLabel;
+    private Label wordLabel;
+    private TextField barre;
+    private int lives;
+    private String wordToGuess;
+    private Set<Character> guessedLetters;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        AtomicInteger nbVies= new AtomicInteger(7);
         BorderPane root = new BorderPane();
-        VBox toutBas=new VBox();
-        //toutes les case avec les numéros
-        HBox PLigne=new HBox();
-        Button a=new Button("a");
-        Button e=new Button("e");
-        Button i=new Button("i");
-        Button o=new Button("o");
-        Button u=new Button("u");
-        Button y=new Button("y");
-        PLigne.getChildren().addAll(a,e,i,o,u,y);
-        PLigne.setAlignment(Pos.CENTER);
-        HBox DLigne=new HBox();
-        Button b=new Button("b");
-        Button c=new Button("c");
-        Button d=new Button("d");
-        Button f=new Button("f");
-        Button g=new Button("g");
-        Button h=new Button("h");
-        Button j=new Button("j");
-        Button k=new Button("k");
-        Button l=new Button("l");
-        Button m=new Button("m");
-        DLigne.getChildren().addAll(b,c,d,f,g,h,j,k,l,m);
-        DLigne.setAlignment(Pos.CENTER);
-        HBox TLigne=new HBox();
-        Button n=new Button("n");
-        Button p=new Button("p");
-        Button q=new Button("q");
-        Button r=new Button("r");
-        Button s=new Button("s");
-        Button t=new Button("t");
-        Button v=new Button("v");
-        Button w=new Button("w");
-        Button x=new Button("x");
-        Button z=new Button("z");
-        TLigne.getChildren().addAll(n,p,q,r,s,t,v,w,x,z);
-        TLigne.setAlignment(Pos.CENTER);
-        HBox boutonBas=new HBox();
-        Button Rejouer=new Button("Rejouer");
-        boutonBas.getChildren().add(Rejouer);
-        boutonBas.setAlignment(Pos.CENTER);
+        VBox lettre = new VBox();
+        HBox rejouer=new HBox();
+        Button retryButton = new Button("Rejoue Pélo");
+        retryButton.setOnAction(event -> setupGame());
+        rejouer.getChildren().addAll(retryButton);
+        rejouer.setAlignment(Pos.BOTTOM_CENTER);
 
-        //text field pour choisir la lettre
-        VBox Milieu=new VBox();
-        //Bas Vbox
-        toutBas.getChildren().addAll(PLigne,DLigne,TLigne,boutonBas);
-        root.setBottom(toutBas);
+        Button a = new Button("A");
+        Button e = new Button("E");
+        Button i = new Button("I");
+        Button o = new Button("O");
+        Button u = new Button("U");
+        Button y = new Button("Y");
+        HBox ae = new HBox();
+        ae.getChildren().addAll(a, e, i, o, u, y);
+        ae.setAlignment(Pos.CENTER);
 
-        //nbVie
-        HBox nbViesBox=new HBox();
-        Label nbVieLab=new Label("Nombre de vie :"+nbVies);
-        nbViesBox.getChildren().add(nbVieLab);
-        nbViesBox.setAlignment(Pos.CENTER);
+        Button b = new Button("B");
+        Button c = new Button("C");
+        Button d = new Button("D");
+        Button f = new Button("F");
+        Button g = new Button("G");
+        Button h = new Button("H");
+        Button j = new Button("J");
+        Button k = new Button("K");
+        Button l = new Button("L");
+        Button m = new Button("M");
+        HBox bm = new HBox();
+        bm.getChildren().addAll(b, c, d, f, g, h, j, k, l, m);
+        bm.setAlignment(Pos.CENTER);
 
-        //mot a deviner
-        HBox mot=new HBox();
-        String motCache;
-        Dico CreationMot=new Dico();
-        motCache=CreationMot.getMot();
-        String motEtoile=new String();
-        String finalMotCache = motCache;
-        for (int I=0;I<motCache.length();I++){
-            motEtoile=motEtoile+"*";
-        }
+        Button n = new Button("N");
+        Button p = new Button("P");
+        Button q = new Button("Q");
+        Button r = new Button("R");
+        Button s = new Button("S");
+        Button t = new Button("T");
+        Button v = new Button("V");
+        Button w = new Button("W");
+        Button x = new Button("X");
+        Button z = new Button("Z");
+        HBox nz = new HBox();
+        nz.getChildren().addAll(n, p, q, r, s, t, v, w, x, z);
+        nz.setAlignment(Pos.CENTER);
+        Image image=new Image("../../resources/exercice6/pendu7.png");
+        ImageView imageView = new ImageView(image);
+        lettre.getChildren().addAll(imageView,ae, bm, nz,rejouer);
+        root.setBottom(lettre);
 
-        AtomicReference<String> finalMotEtoile = new AtomicReference<>(motEtoile);
-        Label motEtoileComp=new Label(finalMotEtoile.get());
+        livesLabel = new Label();
+        wordLabel = new Label();
+        wordLabel.setStyle("-fx-font-size: 20px;");
 
-        Label TESTAENLEVER=new Label(finalMotCache);
 
-        mot.getChildren().add(motEtoileComp);
+        VBox mot = new VBox(10, livesLabel, wordLabel);
         mot.setAlignment(Pos.CENTER);
+        root.setCenter(mot);
 
-        //Vbox milieu
-        Milieu.getChildren().addAll(nbViesBox,mot,TESTAENLEVER);
-        root.setCenter(Milieu);
+        a.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('A'));
+        b.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('B'));
+        c.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('C'));
+        d.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('D'));
+        e.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('E'));
+        f.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('F'));
+        g.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('G'));
+        h.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('H'));
+        i.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('I'));
+        j.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('J'));
+        k.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('K'));
+        l.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('L'));
+        m.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('M'));
+        n.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('N'));
+        o.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('O'));
+        p.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('P'));
+        q.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('Q'));
+        r.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('R'));
+        s.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('S'));
+        t.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('T'));
+        u.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('U'));
+        v.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('V'));
+        w.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('W'));
+        x.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('X'));
+        y.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('Y'));
+        z.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> handleGuess('Z'));
 
+        setupGame();
 
-        a.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
-                if (CreationMot.getPositions('a', finalMotCache).isEmpty()){
-                    nbVies.decrementAndGet();
-                    nbVieLab.setText("Nombre de vie :"+nbVies);
-                }
-                else {
-                    for (int I=0;I<CreationMot.getPositions('a', finalMotCache).size();I++){
-                        int charAModif= (CreationMot.getPositions('a', finalMotCache)).get(I);
-                        finalMotEtoile.set(finalMotEtoile.get().substring(0, charAModif) + 'a' + finalMotEtoile.get().substring(charAModif + 1));
-                        motEtoileComp.setText(finalMotEtoile.get());
-
-                    }
-
-
-                }
-            if (finalMotEtoile.equals(finalMotCache)){
-                System.out.println("gagné");
-                Platform.exit();
-            }
-                if (nbVies.get() ==0){
-                    Platform.exit();
-                }
-        });
-        e.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
-            if (CreationMot.getPositions('e', finalMotCache).isEmpty()){
-                nbVies.decrementAndGet();
-                nbVieLab.setText("Nombre de vie :"+nbVies);
-            }
-            else {
-                for (int I=0;I<CreationMot.getPositions('e', finalMotCache).size();I++){
-                    int charAModif= (CreationMot.getPositions('e', finalMotCache)).get(I);
-                    finalMotEtoile.set(finalMotEtoile.get().substring(0, charAModif) + 'e' + finalMotEtoile.get().substring(charAModif + 1));
-                    motEtoileComp.setText(finalMotEtoile.get());
-
-                }
-
-            }
-            if (finalMotEtoile.equals(finalMotCache)){
-                System.out.println("gagné");
-                Platform.exit();
-            }
-            if (nbVies.get() ==0){
-                Platform.exit();
-            }
-        });
-
-
-
-        root.setStyle("-fx-background-color: TURQUOISE");
-        Scene scene =new Scene(root);
         primaryStage.setTitle("Jeu du pendu");
         primaryStage.setWidth(500);
         primaryStage.setHeight(550);
+        root.setStyle("-fx-background-color: turquoise; -fx-text-fill: white;");
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        // A completer
-
         primaryStage.show();
+    }
+
+    private void setupGame() {
+        guessedLetters = new HashSet<>();
+        lives = 7;
+
+        Dico dico = new Dico();
+        wordToGuess = dico.getMot().toUpperCase();
+        updateWordLabel();
+        updateLivesLabel();
+    }
+
+    private void handleGuess(char letter) {
+        if (guessedLetters.contains(letter)) {
+            return; // Skip if already guessed
+        }
+
+        guessedLetters.add(letter);
+
+        if (wordToGuess.contains(String.valueOf(letter))) {
+            updateWordLabel();
+        } else {
+            lives--;
+            updateLivesLabel();
+        }
+
+        checkGameStatus();
+    }
+
+    private void updateWordLabel() {
+        StringBuilder displayedWord = new StringBuilder();
+        for (char letter : wordToGuess.toCharArray()) {
+            if (guessedLetters.contains(letter)) {
+                displayedWord.append(letter);
+            } else {
+                displayedWord.append('*');
+            }
+        }
+        wordLabel.setText(displayedWord.toString());
+    }
+
+    private void updateLivesLabel() {
+        livesLabel.setText("Nombre de vies : " + lives);
+    }
+
+    private void checkGameStatus() {
+        if (lives <= 0) {
+            wordLabel.setText(wordToGuess);
+            barre.setText("Perdu!");
+        } else if (wordLabel.getText().equals(wordToGuess)) {
+            barre.setText("Gagné!");
+        }
     }
 
     public static void main(String[] args) {
